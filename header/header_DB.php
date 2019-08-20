2fc15524f17a3854b5db5b26db17488e67316705
peanutoon <nexcube@nexcube.co.kr>
2019-08-20 20:15:32 +0900

    uat 용 url 추가

--- header/header_DB.php
+++ header/header_DB.php
@@ -7,4 +7,8 @@
 	$sandbox_enroll_url = "https://acc-ipt.apple.com/enroll-service/1.0/bulk-enroll-devices";
 	$sandbox_check_url = "https://acc-ipt.apple.com/enroll-service/1.0/check-transaction-status";
 	$sandbox_orderdetail_url = "https://acc-ipt.apple.com/enroll-service/1.0/show-order-details";
+
+	$uat_enroll_url = "https://api-applecareconnect-ept.apple.com/enroll-service/1.0/bulk-enroll-devices";
+	$uat_check_url = "https://api-applecareconnect-ept.apple.com/enroll-service/1.0/check-transaction-status";
+	$uat_orderdetail_url = "https://api-applecareconnect-ept.apple.com/enroll-service/1.0/show-order-details";
 ?>
