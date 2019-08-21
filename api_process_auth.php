ee32476b009384eeb78f493e3d38c048f5947ca3
peanutoon <nexcube@nexcube.co.kr>
2019-08-21 16:22:23 +0900

    auth 테스트 02

--- api_process_auth.php
+++ api_process_auth.php
@@ -6,7 +6,7 @@
 $pemfile = "/etc/ssl/certs/GRX-0001192168.ACC1914.Test.AppleCare.chain.pem";
 $keyfile = "/home/sknb2b/dep_test_key/privatekey.key";
 $url = "https://api-applecareconnect-ept.apple.com/enroll-service/1.0/check-transaction-status";
-$requestXml = "requestData";
+$requestXml = "";
 
 $ch = curl_init(); 
 curl_setopt($ch, CURLOPT_URL, $url); 
@@ -20,10 +20,10 @@ curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
 curl_setopt($ch, CURLOPT_SSLKEY, $keyfile); 
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $requestXml);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $requestXml);
 $ret = curl_exec($ch);
 
-echo $ret;
+var_dump($ret);
 
 /*
 
