9a28d68edcd54becd6bbd80b6473cbf671ed1a4c
peanutoon <nexcube@nexcube.co.kr>
2019-09-06 13:31:55 +0900

    1

--- order.php
+++ order.php
@@ -72,6 +72,7 @@
 
 								$error_view = "";
 								$sql = "SELECT count(*) FROM t_api_check_result WHERE t_order_idx = $row[idx] AND errCode != '' AND errCode != null";
+								echo $sql . "<br>";
 								$rowError = x_FETCH($sql, $cntDB);
 								if ($rowError[0] != 0) {
 									$error_view = "<a href='error_detail.php?idx=<?=$row[idx]?>' onclick='popupOpen(event, this.href, \"errorView\", 500, 450)'>";
